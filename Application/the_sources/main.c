d2642f52dee0f1d31e3d390e2226d9f7057ae557
MrJown <naes.lagrange@gmail.com>
2018-03-15 12:07:45 +0100

    nat

--- Application/the_sources/main.c
+++ Application/the_sources/main.c
@@ -9,16 +9,17 @@ int main (int argc, char *argv[]) {
 
   fichier = fopen (argv[1], "r");
 
-  if (fichier == NULL)  {
+/*  if (fichier == NULL)  {
       printf("Impossible d'ouvrir le fichier %s", argv[1]);
       exit(0); // Arret du programme
   }
+  */
 
-  char typeimg;
-  typeimg = (char)fgetc(fichier);
-  printf("%c", typeimg);
+  char typeimg[2] = "";
+  fscanf(fichier, "%c%c", &typeimg[0], &typeimg[1]);
+  printf("%c%c\n", typeimg[0], typeimg[1]);
   //int P    = fscanf(fichier, "%d", &P3);
 
-
+fclose(fichier);
   return 0;
 }
